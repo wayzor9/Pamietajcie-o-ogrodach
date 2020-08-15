9f5a8a8082b8c962de043ba89f2dc8c0315e1141
wayzor9 <wayzor9@gmail.com>
2020-08-16 00:58:42 +0200

    admin registration

--- plants/admin.py
+++ plants/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
+from .models import Plant, ProfilePlant, CommonName, PlantPicture, Description
+
 # Register your models here.
+
+
+@admin.register(Plant)
+class PlantAdmin(admin.ModelAdmin):
+    list_display = ("name", "plantId_id")
+    search_fields = ("name",)
+
+
+@admin.register(ProfilePlant)
+class ProfilePlantAdmin(admin.ModelAdmin):
+    list_display = ("plant", "user")
+    ordering = ("plant",)
+
+
+@admin.register(PlantPicture)
+class PlantPictureAdmin(admin.ModelAdmin):
+    list_display = ("plant", "profile_plant")
+    search_fields = ("plant",)
+
+
+admin.site.register(CommonName, Description)
