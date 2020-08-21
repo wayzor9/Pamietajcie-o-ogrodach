29266c1243530ad8fc522aaf8d59b891e11f7426
wayzor9 <wayzor9@gmail.com>
2020-08-21 20:07:36 +0200

    add API snippets

--- plantID/utils.py
+++ plantID/utils.py
@@ -1,4 +1,6 @@
+import base64
 import requests
+
 from django.conf import settings
 
 PLANTID_API_KEY = getattr(settings, "PLANTID_API_KEY", None)
@@ -16,4 +18,35 @@ class PlantIdClient:
         response = requests.post(
             endpoint, headers=headers, auth=("client", self.api_key)
         )
+        return response.json()
+
+    def encode_files(self, file_names):
+        files_encoded = []
+        for file_name in file_names:
+            with open(file_name, "rb") as file:
+                files_encoded.append(base64.b64encode(file.read()).decode("ascii"))
+        return files_encoded
+
+    def identify_plant(self, file_names):
+        images = self.encode_files(file_names)
+
+        params = {
+            "api_key": self.api_key,
+            "images": images,
+            "modifiers": ["crops_fast", "similar_images"],
+            "plant_language": "en",
+            "plant_details": [
+                "common_names",
+                "url",
+                "name_authority",
+                "wiki_description",
+                "taxonomy",
+                "synonyms",
+            ],
+        }
+
+        headers = {"Content-Type": "application/json"}
+
+        response = requests.post(self.identification_url, json=params, headers=headers)
+
         return response.json()
